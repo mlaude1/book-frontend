debbe78667cfe9809ad9fb5e402ac160b714364e
sarahecarter <secarter213@gmail.com>
2021-11-16 15:09:21 -0500

    completed front end setup and routes

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom"
+
+const Header = (props) => {
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <div>
+                    <h1>Bookmark'd</h1>
+                </div>
+            </Link>
+        </nav>
+    )
+}
+
+export default Header
---
+++ src/pages/Index.js
@@ -0,0 +1,5 @@
+const Index = (props) => {
+    return <h1>This is the index page</h1>
+}
+
+export default Index
