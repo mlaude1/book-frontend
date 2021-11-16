24c139fe8e8b4d0b7cb0a0963eec50f2921c38b0
Matthew Laude <laude_matthew@yahoo.com>
2021-11-16 16:50:23 -0500

    completed frontend index

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,10 +4,35 @@ import Index from "../pages/Index"
 import Show from "../pages/Show"
 
 const Main = (props) => {
+    
+    const [books, setBooks] = useState(null)
+    const URL = "https://bsm-book-backend.herokuapp.com/books/"
+    
+    const getBooks = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setBooks(data)
+    }
+
+    const createBook = async (newBook) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newBook)
+        })
+        getBooks()
+    }
+
+    useEffect(() => getBooks(), [])
+
     return (
         <main>
             <Routes>
-                <Route path="/" element={<Index />}/>
+                <Route path="/" element={
+                <Index books={books} createBook={createBook}/>
+                }/>
                 <Route path="/books/:id" element={<Show />}/>
             </Routes>
         </main>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,5 +1,101 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 const Index = (props) => {
-    return <h1>This is the index page</h1>
+    
+    const [newForm, setNewForm] = useState({
+        title: "",
+        author: "",
+        genre: "",
+        coverImage: "",
+        description: ""
+    })
+
+    const handleChange = (event) => {
+        const newState = {...newForm}
+        newState[event.target.name] = event.target.value
+        setNewForm(newState)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.createBook(newForm)
+        setNewForm({
+            title: "",
+            author: "",
+            genre: "",
+            coverImage: "",
+            description: ""
+        })
+    }
+
+    const form = (
+        <form onSubmit={handleSubmit}>
+            <legend>Add a new book!</legend>
+            <input 
+                type="text"
+                value={newForm.title}
+                name="title"
+                placeholder="Book Title"
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                value={newForm.author}
+                name="author"
+                placeholder="Author"
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                value={newForm.genre}
+                name="genre"
+                placeholder="Genre"
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                value={newForm.coverImage}
+                name="coverImage"
+                placeholder="Book Cover Image"
+                onChange={handleChange}
+            />
+            <input 
+                type="text"
+                value={newForm.description}
+                name="description"
+                placeholder="Brief Description"
+                onChange={handleChange}
+            />
+            <input type="submit" value="Add Book" />
+        </form>
+    )
+
+    if (props.books) {
+        return (
+            <section>
+                {form}
+                <div className="books-container">
+                    {props.books.map((book) => {
+                        return (<div key={book._id} className = "book">
+                            <Link to={`/books/${book._id}`}>
+                                <img src={book.coverImage}/>
+                                <h1>{book.title}</h1>
+                                <h2>by {book.author}</h2>
+                            </Link>
+                        </div>)
+                    })}
+                </div>
+            </section>
+        )
+    } else {
+        return (
+            <section>
+                {form}
+                <h1>Loading...</h1>
+            </section>
+        )
+    }
 }
 
 export default Index
