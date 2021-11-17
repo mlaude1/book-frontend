f40410414031e9c4007cfc5da5198a1ca4654569
sarahecarter <secarter213@gmail.com>
2021-11-17 12:02:40 -0500

    added update and delete functionality to show page

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,12 +8,14 @@ const Main = (props) => {
     const [books, setBooks] = useState(null)
     const URL = "https://bsm-book-backend.herokuapp.com/books/"
     
+    // Calls backend API to get book list
     const getBooks = async () => {
         const response = await fetch(URL);
         const data = await response.json();
         setBooks(data)
     }
 
+    // Adds a new book 
     const createBook = async (newBook) => {
         await fetch(URL, {
             method: "post",
@@ -21,6 +23,26 @@ const Main = (props) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(newBook)
+        })
+        getBooks()
+    }
+
+    // Updates a book
+    const updateBook = async (book, id) => {
+        await fetch(URL + id, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(book)
+        })
+        getBooks()
+    }
+
+    // Deletes a book 
+    const deleteBook = async (id) => {
+        await fetch(URL + id, {
+            method: "delete"
         })
         getBooks()
     }
@@ -33,7 +55,7 @@ const Main = (props) => {
                 <Route path="/" element={
                 <Index books={books} createBook={createBook}/>
                 }/>
-                <Route path="/books/:id" element={<Show />}/>
+                <Route path="/books/:id" element={<Show books={books} updateBook={updateBook} deleteBook={deleteBook}/>}/>
             </Routes>
         </main>
     )
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,105 @@
+import { useParams, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react";
+
 const Show = (props) => {
-    return <h1>This is the show page</h1>
+    const navigate = useNavigate();
+    const params = useParams();
+    const id = params.id;
+    const books = props.books;
+
+    // state for edit form 
+    const [editForm, setEditForm] = useState({})
+    // useEffect to set state for existing book when data is available
+    useEffect(() => {
+        if (props.books) {
+            // grab the specific book
+            const book = books.find((b) => b._id === id)
+            setEditForm(book)
+        }
+    }, [props.books])
+
+
+    if (props.books) {
+        // grab the specific book
+        const book = books.find((b) => b._id === id)
+
+        // handleChange function for form 
+        const handleChange = (event) => {
+            const newState = {...editForm}
+            newState[event.target.name] = event.target.value
+            setEditForm(newState)
+        }
+
+        // handleSubmit function for form 
+        const handleSubmit = (event) => {
+            event.preventDefault()
+            props.updateBook(editForm, book._id)
+            navigate("/")
+        }
+
+        // removes a book
+        const removeBook = () => {
+            props.deleteBook(book._id)
+            navigate("/")
+        }
+
+        const form = (
+            <form onSubmit={handleSubmit}>
+                <legend>Edit book details:</legend>
+                <input 
+                    type="text"
+                    value={editForm.title}
+                    name="title"
+                    placeholder="Book Title"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    value={editForm.author}
+                    name="author"
+                    placeholder="Author"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    value={editForm.genre}
+                    name="genre"
+                    placeholder="Genre"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    value={editForm.coverImage}
+                    name="coverImage"
+                    placeholder="Book Cover Image"
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    value={editForm.description}
+                    name="description"
+                    placeholder="Brief Description"
+                    onChange={handleChange}
+                />
+                <input type="submit" value="Update Book" />
+            </form>
+        )
+
+        return (
+            <div className="book-show">
+                <h1>{book.title}</h1>
+                <h2>{book.author}</h2>
+                <img src={book.coverImage} alt={book.title}/>
+                <h4>{book.description}</h4>
+                <p>{book.genre}</p>
+                {form}
+                <button onClick={removeBook}>Delete Book</button>
+            </div>
+        )
+    }
+    else {
+        return <h1>No Book Found</h1>
+    }
 }
 
 export default Show
