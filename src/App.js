db5fd8f6098ff6b56d1cc7b3000cb2c42b1c3a83
Matthew Laude <laude_matthew@yahoo.com>
2021-11-19 06:44:59 -0500

    added carousel (draft)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import ImageSlider from "./components/ImageSlider";
 import Main from "./components/Main";
+import { SliderData } from "./components/SliderData";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ImageSlider slides={SliderData} />
       <Main />
     </div>
   );
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import { SliderData } from './SliderData';
+
+const ImageSlider = ({ slides }) => {
+  const [current, setCurrent] = useState(0)
+  const length = slides.length
+  
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1)
+  }
+
+  if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <section className="slider">
+      <i class="fa fa-chevron-circle-left" aria-hidden="true" id="left-arrow" onClick={prevSlide}></i>
+      <i class="fa fa-chevron-circle-right" aria-hidden="true" id="right-arrow" onClick={nextSlide}></i>
+      {SliderData.map((slide, index) => {
+        return (
+          // <div 
+          // className={index === current ? "slide active" : "slide"}
+          // key={index}
+          // >
+          <div 
+          className="slide"
+          key={index}
+          >
+            {index === current && (
+            <img src={slide.image} alt="carousel image" className="image" />
+            )}
+          </div>
+        )
+      })}
+    </section>
+  )
+};
+
+export default ImageSlider
