9eb5047b990b4593d5ce619978b9e652cb2274be
Matthew Laude <laude_matthew@yahoo.com>
2021-11-19 17:31:53 -0500

    added images in carousel

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,5 +1,14 @@
 export const SliderData = [
   {
+    image: 'https://i.imgur.com/2LLrgXL.png'
+  },
+  {
+    image: 'https://i.imgur.com/CnZgXoV.png'
+  },
+  {
+    image: 'https://i.imgur.com/ftBX534.png'
+  },
+  {
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs-media-cache-ak0.pinimg.com%2Foriginals%2F1a%2Fff%2F0b%2F1aff0bc40b3a4e06fbf515fc69b24fb7.jpg&f=1&nofb=1'
   },
   {
@@ -15,6 +24,6 @@ export const SliderData = [
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.sandiegoreader.com%2Fimg%2Fevents%2F2015%2Fclifford-book-fair.jpg&f=1&nofb=1'
   },
   {
-    image: 'https://dispatch.barnesandnoble.com/content/dam/ccr/homepage/daily/2021/11/16/Gateway_Quote_A2_black-voices_11-16.jpg'
+    image: 'https://i.imgur.com/X1FVD9s.png'
   }
 ]
